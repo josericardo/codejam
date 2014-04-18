c3ca834b25b3b45c3a334e77772e67c538c0fc5a
José Ricardo <zehzinho@gmail.com>
2014-04-18 14:23:24 -0300

    Adding timing and removing auxiliary function.
    Time: 0.17s

--- cookieclicker/cookieclicker.py
+++ cookieclicker/cookieclicker.py
@@ -2,29 +2,36 @@
 # coding=utf-8
 
 import fileinput
+import time
+import numba
+
+def elapsed_time(rate, C, F, X):
+    elapsed_time = 0.0
+
+    worth = True
+    while worth:
+        time_to_reach_X = X / rate
+        time_to_build_another_farm = C / rate
+        time_to_reach_X_in_the_new_rate = X / (rate + F)
+        worth = time_to_reach_X > time_to_build_another_farm + time_to_reach_X_in_the_new_rate
+
+        if worth:
+            elapsed_time += C / rate # time to build another farm
+            rate += F
+
+    # by now I've burned all cookies building new farms
+    return elapsed_time + (X / rate)
+
+start = time.time()
 
 data = (l for l in fileinput.input())
 T = int(data.next())
-
-def worth_build_another_farm(rate, C, F, X):
-    time_to_reach_X = X / rate
-    time_to_build_another_farm = C / rate
-    time_to_reach_X_in_the_new_rate = X / (rate + F)
-
-    return time_to_reach_X > time_to_build_another_farm + time_to_reach_X_in_the_new_rate
 
 for i in xrange(1, T+1):
     C, F, X = [float(f) for f in data.next().split()]
 
     rate = 2.0 # cookies/sec
-    elapsed_time = 0.0
+    answer = elapsed_time(rate, C, F, X)
+    #print('Case #%d: %.7f' % (i, answer))
 
-    while worth_build_another_farm(rate, C, F, X):
-        elapsed_time += C / rate # time to build another farm
-        rate += F
-
-    # by now I've burned all cookies building new farms
-    elapsed_time += X / rate
-
-    answer = elapsed_time
-    print('Case #%d: %.7f' % (i, answer))
+print "Finished in ", time.time()-start, " secs."
